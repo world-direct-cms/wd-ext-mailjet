e1ccf6a2a014d08dfa43f2f5327e5477dc047711
Klaus HÃrmann-Engl <Klaus.Hoermann-Engl@world-direct.at>
2026-02-16 12:37:07 +0100

    task: initial version 0.0.1

---
+++ README.md
@@ -0,0 +1,19 @@
+<!-- omit in toc -->
+# TYPO3 extension: "mailjet"
+
+<!-- omit in toc -->
+## Content
+
+## What does it do?
+This extension configures TYPO3 to use `Mailjet` for sending emails. Also it comes with a database table, in which all email sending actions are inserted, so that you can see a statistics about the email sending process.
+
+## Installation
+The extension can easily be installed using composer:
+```bash
+composer install worlddirect/mailjet
+```
+
+## Configuration
+In order for sending mails through `Mailjet` works, you need to set certain extension configuration settings:
+
+-
---
+++ ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext: "mailjet"
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Mailjet Extension',
+    'description' => 'Diese Erweiterung konfiguriert das System zur Verwendung mit Mailjet. Ausserdem wird eine Datenbank Tabelle erstellt, welche alle versendeten E-Mails speichert. Jedoch ohne Datenschutz relevante Informationen.',
+    'category' => 'plugin',
+    'author' => 'Klaus Hörmann-Engl',
+    'author_email' => 'kho@world-direct.at',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.0.0-14.9.99'
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
